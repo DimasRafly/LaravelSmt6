59fee05b53837fc01c370ced121b10b573792c22
DimasRafly <dimasrafly17@gmail.com>
2023-05-12 23:59:46 +0700

    galeri_card_rev

--- resources/views/galeri.blade.php
+++ resources/views/galeri.blade.php
@@ -4,10 +4,13 @@
 <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Dimas Rafly's Gallery</title>
+  
   <!-- Load Bootstrap CSS -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
   
   <!-- Load CSS file -->
+  <link rel="stylesheet" href="{{ asset('assets/css/style.css')}}">
+
 </head>
 <body>
 
@@ -35,110 +38,111 @@
       </div>
     </nav>
 
-<!-- Container -->
+<!-- CONTAINER 1 -->
 
-    <div class="container mt-5">
-      <div class="row">
-        <div class="col-lg-4 mb-3">
-          <div class="card">
-            <img src="{{ asset('assets/img/muamalat.jpg')}}" class="card-img-top" alt="..." width="300" height="200">
-            <div class="card-body">
-              <h5 class="card-title">Business Intelligence Analyst</h5>
-              <p class="card-text">In this project-based internship, I, as a business intelligence analyst, am assigned to process data and create a sales dashboard.</p>
-              <a href="https://bit.ly/VIX_BI_Analyst_DashboardGDS" target = "_blank" class="btn btn-primary">View Project</a>
-            </div>
-          </div>
-        </div>
-        <div class="col-lg-4 mb-3">
-          <div class="card">
-            <img src="{{ asset('assets/img/btpns.jpeg')}}" class="card-img-top" alt="..." width="300" height="200">
-            <div class="card-body">
-              <h5 class="card-title">Data Engineer</h5>
-              <p class="card-text">In this project-based internship as a data engineer, my tasks are determining business objectives, conducting data exploration, delivering insight presentations, and providing conclusions and recommendations.</p>
-              <a href="https://drive.google.com/file/d/1GpErDnsneymd0iXqsbwl4rPN_001aq-o/view?usp=share_link" target = "_blank" class="btn btn-primary">View Project</a>
-            </div>
-          </div>
-        </div>
-        <div class="col-lg-4 mb-3">
-          <div class="card">
-          <img src="{{ asset('assets/img/rakamin.png')}}" class="card-img-top" alt="..." width="300" height="200">
-            <div class="card-body">
-              <h5 class="card-title">Data Science</h5>
-              <p class="card-text">The task in this project is to measure online retail, to determine how well an online store or e-commerce business is selling products or services on digital platforms.</p>
-              <a href="https://drive.google.com/file/d/1lRqXK3JUBHfOWOr-pPO2FpwXCabV_2e1/view?usp=share_link" target = "_blank" class="btn btn-primary">View Project</a>
-            </div>
-          </div>
-        </div>
+<div class="container">
+  <div class="d-flex justify-content-around align-items-start flex-wrap">
+    <div class="card">
+      <div class= "card-image">
+        <img src="{{ asset('assets/img/muamalat.jpg')}}" class="card-img-top" alt="...">
       </div>
-    </div>
-    <div class="container mt-5">
-      <div class="row">
-        <div class="col-lg-4 mb-3">
-          <div class="card">
-            <img src="{{ asset('assets/img/fifa.png') }}" class="card-img-top" alt="...">
-            <div class="card-body">
-              <h5 class="card-title">Analytics with SQL and Python</h5>
-              <p class="card-text">This project aims to find the top data in several variables, such as the top 5 player's salary, top 5 Germany players salary, and so on.</p>
-              <a href="https://drive.google.com/file/d/1NrG4Fj5y2dGsxaYke4qcSQ7Sa8Auoi4A/view?usp=share_link" target="_blank" class="btn btn-primary">View Project</a>
-            </div>
-          </div>
-        </div>
-        <div class="col-lg-4 mb-3">
-          <div class="card">
-            <img src="{{ asset('assets/img/bwa.jpg') }}" class="card-img-top" alt="..." width="100" height="250">
-            <div class="card-body">
-              <h5 class="card-title">Data Science : Linear Regression</h5>
-              <p class="card-text">Forecasting the price of pizza based on several variables using a machine learning model.</p>
-              <a href="https://medium.com/@dimasrafly17/data-science-project-predict-pizza-price-using-linear-regression-1989ac87f84" target = "_blank" class="btn btn-primary">View Project</a>
-            </div>
-          </div>
-        </div>
-        <div class="col-lg-4 mb-3">
-          <div class="card">
-            <img src="{{ asset('assets/img/games.png') }}" class="card-img-top" alt="..." width="100" height="250">
-            <div class="card-body">
-              <h5 class="card-title">Data Analytics : Game insights</h5>
-              <p class="card-text">Finding insights that match the given problem statement using Python and visualizing them.</p>
-              <a href="https://colab.research.google.com/drive/1-Y_UdIMWWxcd0hNb9gA__r5zEMLsCqam?usp=share_link" target="_blank" class="btn btn-primary">View Project</a>
-            </div>
-          </div>
-        </div>
+      <div class="card-body">
+        <h5 class="card-title">Business Intelligence Analyst</h5>
+        <p class="card-text">In this project-based internship, I, as a business intelligence analyst, am assigned to process data and create a sales dashboard.</p>
       </div>
+      <a href="https://colab.research.google.com/drive/1lRqXK3JUBHfOWOr-pPO2FpwXCabV_2e1" target = "_blank" class="btn btn-primary">View Project</a>
     </div>
-    <div class="container mt-5">
-      <div class="row">
-        <div class="col-lg-4 mb-3">
-          <div class="card">
-            <img src="{{ asset('assets/img/sklearn.png') }}" class="card-img-top" alt="...">
-            <div class="card-body">
-              <h5 class="card-title">Data Mining : K-Means Implementation</h5>
-              <p class="card-text">Clustering people's names based on their height and weight using the K-Means algorithm. One of the libraries used is scikit-learn.</p>
-              <a href="https://drive.google.com/file/d/1Pftzg8sHtNAd_N-D-iM2YD-BFWd-jY8U/view?usp=share_link" target="_blank" class="btn btn-primary">View Project</a>
-            </div>
-          </div>
-        </div>
-        <div class="col-lg-4 mb-3">
-          <div class="card">
-            <img src="{{ asset('assets/img/cofe.jpg') }}" class="card-img-top" alt="...">
-            <div class="card-body">
-              <h5 class="card-title">Data Analytics : Coffe Shop Sales Dashboard</h5>
-              <p class="card-text">Analyzing trends of coffee shops in New York City, such as their top-selling products, total revenue, and analyzing their customers' habits.</p>
-              <a href="https://colab.research.google.com/drive/1bD3s4Ul64bOYhOzjO9NxHwJHtrkn9_7b?usp=share_link" target="_blank" class="btn btn-primary">View Project</a>
-            </div>
-          </div>
-        </div>
-        <div class="col-lg-4 mb-3">
-          <div class="card">
-            <img src="{{ asset('assets/img/poly.png') }}" class="card-img-top" alt="...">
-            <div class="card-body">
-              <h5 class="card-title">Data Warehouse & Business Intelligence</h5>
-              <p class="card-text">Creating a sales dashboard report to view the performance and sales trends of PT Insera Sena over the past two months.</p>
-              <a href="https://bit.ly/FinalProject_DWBI" target="_blank" class="btn btn-primary">View Project</a>
-            </div>
-          </div>
-        </div>
+    <div class="card">
+      <div class= "card-image">
+        <img src="{{ asset('assets/img/btpns.jpeg')}}" class="card-img-top" alt="...">
       </div>
+      <div class="card-body">
+        <h5 class="card-title">Data Engineer</h5>
+        <p class="card-text">In this project-based internship as a data engineer, my tasks are determining business objectives, conducting data exploration, delivering insight presentations, and providing conclusions and recommendations.</p>
+      </div>
+      <a href="https://colab.research.google.com/drive/1lRqXK3JUBHfOWOr-pPO2FpwXCabV_2e1" target = "_blank" class="btn btn-primary">View Project</a>
     </div>
+    <div class="card">
+      <div class= "card-image">
+        <img src="{{ asset('assets/img/rakamin.png')}}" class="card-img-top" alt="...">
+      </div>
+      <div class="card-body">
+        <h5 class="card-title">Data Science</h5>
+        <p class="card-text">The task in this project is to measure online retail, to determine how well an online store or e-commerce business is selling products or services on digital platforms.</p>
+      </div>
+      <a href="https://colab.research.google.com/drive/1lRqXK3JUBHfOWOr-pPO2FpwXCabV_2e1" target = "_blank" class="btn btn-primary">View Project</a>
+    </div>
+
+    <!-- CONTAINER 2 -->
+
+    <div class="card">
+      <div class= "card-image">
+      <img src="{{ asset('assets/img/fifa.png') }}" class="card-img-top" alt="...">
+      </div>
+      <div class="card-body">
+        <h5 class="card-title">Analytics with SQL and Python</h5>
+        <p class="card-text">This project aims to find the top data in several variables, such as the top 5 player's salary, top 5 Germany players salary, and so on.</p>
+      </div>
+      <a href="https://colab.research.google.com/drive/1NrG4Fj5y2dGsxaYke4qcSQ7Sa8Auoi4A" target = "_blank" class="btn btn-primary">View Project</a>
+    </div>
+    <div class="card">
+      <div class= "card-image">
+      <img src="{{ asset('assets/img/bwa.jpg') }}" class="card-img-top" alt="...">
+      </div>
+      <div class="card-body">
+        <h5 class="card-title">Data Science : Linear Regression</h5>
+        <p class="card-text">Forecasting the price of pizza based on several variables using a machine learning model.</p>
+      </div>
+      <a href="https://medium.com/@dimasrafly17/data-science-project-predict-pizza-price-using-linear-regression-1989ac87f84" target = "_blank" class="btn btn-primary">View Project</a>
+    </div>
+    <div class="card">
+      <div class= "card-image">
+      <img src="{{ asset('assets/img/games.png') }}" class="card-img-top" alt="...">
+      </div>
+      <div class="card-body">
+        <h5 class="card-title">Data Analytics : Game insights</h5>
+        <p class="card-text">Finding insights that match the given problem statement using Python and visualizing them.</p>
+      </div>
+      <a href="https://medium.com/@dimasrafly17/series-game-insights-fc4a84733028" target = "_blank" class="btn btn-primary">View Project</a>
+    </div>
+
+    <!-- CONTAINER 3 -->
+
+    <div class="card">
+      <div class= "card-image">
+        <img src="{{ asset('assets/img/sklearn.png')}}" class="card-img-top" alt="...">
+      </div>
+      <div class="card-body">
+        <h5 class="card-title">Data Mining : K-Means Implementation</h5>
+        <p class="card-text">Clustering people's names based on their height and weight using the K-Means algorithm. One of the libraries used is scikit-learn.</p>
+      </div>
+      <a href="https://colab.research.google.com/drive/1Pftzg8sHtNAd_N-D-iM2YD-BFWd-jY8U" target = "_blank" class="btn btn-primary">View Project</a>
+    </div>
+    <div class="card">
+      <div class= "card-image">
+        <img src="{{ asset('assets/img/cofe.jpg') }}" class="card-img-top" alt="...">
+      </div>
+      <div class="card-body">
+        <h5 class="card-title">Data Analytics : Coffe Shop Sales Dashboard</h5>
+        <p class="card-text">Analyzing trends of coffee shops in New York City, such as their top-selling products, total revenue, and analyzing their customers' habits.</p>
+      </div>
+      <a href="https://colab.research.google.com/drive/1bD3s4Ul64bOYhOzjO9NxHwJHtrkn9_7b?usp=share_link" target = "_blank" class="btn btn-primary">View Project</a>
+    </div>
+    <div class="card">
+      <div class= "card-image">
+        <img src="{{ asset('assets/img/poly.png') }}" class="card-img-top" alt="...">
+      </div>
+      <div class="card-body">
+        <h5 class="card-title">Data Warehouse & Business Intelligence</h5>
+        <p class="card-text">Creating a sales dashboard report to view the performance and sales trends of PT Insera Sena over the past two months.</p>
+      </div>
+      <a href="https://bit.ly/FinalProject_DWBI" target = "_blank" class="btn btn-primary">View Project</a>
+    </div>
+  </div>
+</div>
+
+<!-- END -->
+
   <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.1.1/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
